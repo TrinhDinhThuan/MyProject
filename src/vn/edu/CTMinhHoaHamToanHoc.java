ece2d6e819efccd7ad6d6ecd5225da029d6974e0
TrinhDinhThuan <91036695+TrinhDinhThuan@users.noreply.github.com>
2021-12-31 12:40:59 +0700

    Delete src/vn/edu directory

--- src/vn/edu/CTMinhHoaHamToanHoc.java
+++
@@ -1,7 +0,0 @@
-package vn.edu;
-
-public class CTMinhHoaHamToanHoc {
-    public static void main(String[] args) {
-        
-    }
-}
--- src/vn/edu/tuan05/CTMinhHoaMang.java
+++
@@ -1,7 +0,0 @@
-package vn.edu.tuan05;
-
-public class CTMinhHoaMang {
-    public static void main(String[] args) {
-        int [] mangSoNguyen = new int[10];
-    }
-}
