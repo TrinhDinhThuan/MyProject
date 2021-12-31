01763d7b3f4565db7e9bd7f014647e290aaf0236
TrinhDinhThuan <91036695+TrinhDinhThuan@users.noreply.github.com>
2021-12-31 12:41:09 +0700

    Delete src/tuhocjava directory

--- src/tuhocjava/hangso.java
+++
@@ -1,9 +0,0 @@
-package tuhocjava;
-
-public class hangso {
-    public static void main(String[] args) {
-        final int x = 100;
-        System.out.println("x = " +x);
-        //khai báo hằng số duy nhất, sẽ bị lỗi nếu cố tình thay đổi giá trị
-    }
-}
